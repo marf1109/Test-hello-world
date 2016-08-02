7c291f3d918880e14552251512bfd04dad15a548
fred <marf1109@gmail.com>
2016-08-02 12:51:46 -0400

    test1

---
+++ ClassLibrary1/Hello.vb
@@ -0,0 +1,5 @@
+﻿Public Class Hello
+    Public Function HelloWorld() As String
+        Return "Hello world!"
+    End Function
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim t As New ClassLibrary1.Hello
+        MessageBox.Show(t.HelloWorld, "test", MessageBoxButtons.OK)
+    End Sub
+End Class
